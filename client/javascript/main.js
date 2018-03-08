14f4175c6148bc03807f90553b15465e0a1c480d
Otto <o.stegmaier@gmail.com>
2018-03-08 07:21:39 -0800

    Working signup page and login page.

--- client/javascript/main.js
+++ client/javascript/main.js
@@ -14,14 +14,38 @@
       context.render('views/login.html').appendTo(context.$element());
     });
 
+  this.post('#/login/', function(context) {
+      const reqData = {
+        email: this.params['user-email'],
+        password: this.params['user-password'],
+      };
+      const reqSettings = {
+          data : JSON.stringify(reqData),
+          contentType : 'application/json',
+          type : 'POST',
+      };
+      $.ajax('/auth/login', reqSettings)
+        .done(function(resData) {
+          console.log(resData);
+          // TODO - Add user email to template.
+          // TODO - Modify nav to reflect logged in user.
+          context.app.swap('');
+          context.render('views/login-success.html')
+            .appendTo(context.$element());
+        })
+        .fail(function(msg) {
+          // Todo - Make nice error message
+          alert(msg.responseText);
+      });
+    });
+
   this.get('#/signup/', function(context) {
       context.app.swap('');
       context.render('views/signup.html').appendTo(context.$element());
     });
 
-  this.post('#/users/', function(context) {
-      console.log(this.params);
-      const cell = this.params['user-cellphone'].replace('-', '').trim()
+  this.post('#/signup/', function(context) {
+      const cell = this.params['user-cellphone'].replace('-', '').trim();
       const reqData = {
         email: this.params['user-email'],
         password: this.params['user-password'],
@@ -36,7 +60,9 @@
         .done(function(resData) {
           console.log(resData);
           context.app.swap('');
-          context.render('views/success.html').appendTo(context.$element());
+          // TODO - Add user email to template.
+          context.render('views/signup-success.html')
+            .appendTo(context.$element());
         })
         .fail(function(msg) {
           // Todo - Make nice error message
